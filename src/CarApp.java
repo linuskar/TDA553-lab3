bd360d191bfa4f72e8f9f962ddefb1a7ac2712db
linuskar <linuskungalv@gmail.com>
2022-12-09 10:51:21 +0100

    moved buttons from view to the CarController class
    Co-authored-by: Caramajau <Caramajau@users.noreply.github.com>
    Co-authored-by: Erik Persson <erikpersson0884@users.noreply.github.com>

--- src/CarApp.java
+++ src/CarApp.java
@@ -13,15 +13,24 @@ public class CarApp {
         CarView carView = new CarView("CarSim", carModel);
         carModel.addObserver(carView);
         // Instance of controller class
-        CarController controller = new CarController(carModel);
-
-
+        CarController controller = new CarController(carModel, carView);
+        controller.makeGasInputSpinner();
+        controller.makeButtonsGrid();
+        controller.makeEngineButtons();
+        
+        controller.addButtonsAsActionListener();
+        carView.setVisible(true);
+      
+        
+  
         carModel.addVehicle(new Volvo240(500, Color.white, 0, 0));
         carModel.addVehicle(new Saab95(300, Color.red, 0, 100));
         carModel.addVehicle(new Scania(400, Color.black, 0, 200));
-
+    
     
         // Start the timer
+        //carModel.update();
         carModel.start();
+
     }
 }
--- src/CarController.java
+++ src/CarController.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 
 import Vehicles.IVehicle;
-
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
+import java.awt.*;
 /*
 * This class represents the Controller part in the MVC pattern.
 * It's responsibilities is to listen to the View and responds in a appropriate manner by
@@ -16,20 +17,112 @@ public class CarController{
     // member fields:
 
     // The frame that represents this instance View of the MVC pattern
-    CarView frame;
+    
     // A list of cars, modify if needed
-    ArrayList<IVehicle> cars = new ArrayList<>();
+    //ArrayList<IVehicle> cars = new ArrayList<>();
 
     private CarModel carModel;
+    private CarView frame;
+
+    private JButton gasButton;
+    private JButton brakeButton;
+    private JButton turboOnButton;
+    private JButton turboOffButton;
+    private JButton liftBedButton;
+    private JButton lowerBedButton;
+    private JPanel controlPanelGrid;
+
+    private JButton startButton;
+    private JButton stopButton;
+
+    private JPanel gasPanel;
+    private JSpinner gasSpinner;
+    private int gasAmount;
+    private JLabel gasLabel;
 
     // Constructor
-    public CarController(CarModel carModel){
+    public CarController(CarModel carModel, CarView carView){
         this.carModel = carModel;
+        this.frame = carView;
+        
+        gasButton = new JButton("Gas");
+        brakeButton = new JButton("Brake");
+        turboOnButton = new JButton("Saab Turbo on");
+        turboOffButton = new JButton("Saab Turbo off");
+        liftBedButton = new JButton("Scania Lift Bed");
+        lowerBedButton = new JButton("Lower Lift Bed");
+        controlPanelGrid = new JPanel();
+        
+        startButton = new JButton("Start all cars");
+        stopButton = new JButton("Stop all cars");
+
+        gasPanel = new JPanel();
+        gasSpinner = new JSpinner();  
+        gasAmount = 0;
+        gasLabel = new JLabel("Amount of gas");
     }
 
 
     //methods:
+    public void makeButtonsGrid(){
+        controlPanelGrid.setLayout(new GridLayout(2,4));
+        controlPanelGrid.add(gasButton, 0);
+        controlPanelGrid.add(turboOnButton, 1);
+        controlPanelGrid.add(liftBedButton, 2);
+        controlPanelGrid.add(brakeButton, 3);
+        controlPanelGrid.add(turboOffButton, 4);
+        controlPanelGrid.add(lowerBedButton, 5);
+        controlPanelGrid.setPreferredSize(new Dimension((frame.getViewX()/2)+4, 200)); 
+        controlPanelGrid.setBackground(Color.CYAN);
+       
+        frame.add(controlPanelGrid);
+        //frame.setVisible(true);
+    }
+    public void makeGasInputSpinner(){
+        SpinnerModel spinnerModel = new SpinnerNumberModel(0, 0, 100, 1);
+        gasSpinner = new JSpinner(spinnerModel);
+        gasSpinner.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                gasAmount = (int) ((JSpinner)e.getSource()).getValue();
+            }
+        });
 
+        gasPanel.setLayout(new BorderLayout());
+        gasPanel.add(gasLabel, BorderLayout.PAGE_START);
+        gasPanel.add(gasSpinner, BorderLayout.PAGE_END);
+        //gasPanel.setPreferredSize(new Dimension((frame.getX()/2), 200));
+        frame.add(gasPanel);
+
+
+        //frame.setVisible(true);
+    }
+
+    public void addButtonsAsActionListener(){
+        // This actionListener is for the gas button only
+        // TODO: Create more for each component as necessary
+        gasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gas(gasAmount);
+            }
+        });
+    }
+
+    public void makeEngineButtons(){
+        startButton.setBackground(Color.blue);
+        startButton.setForeground(Color.green);
+        startButton.setPreferredSize(new Dimension((frame.getViewX()/5)-15,200));
+        
+        frame.add(startButton);
+
+        stopButton.setBackground(Color.red);
+        stopButton.setForeground(Color.black);
+        stopButton.setPreferredSize(new Dimension((frame.getViewX()/5)-15,200));
+
+        frame.add(stopButton);
+
+        //frame.setVisible(true);
+    }
     // public static void main(String[] args) {
     //     // Instance of this class
     //     CarController cc = new CarController();
@@ -49,15 +142,12 @@ public class CarController{
     /* Each step the TimerListener moves all the cars in the list and tells the
     * view to update its images. Change this method to your needs.
     * */
-    //private class TimerListener implements ActionListener {
-        
-    //}
 
     // Calls the gas method for each car once
     void gas(int amount) {
         double gas = ((double) amount) / 100;
-        for (IVehicle car : cars) {
-            car.gas(gas);
+        for (IVehicle vehicle : carModel.getVehicles()) {
+            vehicle.gas(gas);
         }
     }
 
